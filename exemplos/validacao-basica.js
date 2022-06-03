43178fdb00eacfdaf0a0393495f93d953659c89e
ArturBarreto <artur.gomes.barreto@gmail.com>
2022-06-03 13:52:25 -0300

    Aplicação de soma em TS

--- exemplos/validacao-basica.js
+++ exemplos/validacao-basica.js
@@ -5,7 +5,7 @@ let input2 = document.getElementById("input2");
 function somarNumeros(numero1, numero2) {
     if(typeof numero1 === 'number' && typeof numero2 === 'number') {
         return numero1 + numero2;
-    }
+   }
     else {
         return Number(numero1) + Number(numero2);
     }
